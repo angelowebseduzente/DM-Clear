f213bd06282900e1f6a1d6d663d76c61db4b2d41
angelowebseduzente <87149163+angelowebseduzente@users.noreply.github.com>
2021-08-25 18:38:43 +0100

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,44 @@
+﻿const Discord = require('discord.js');
+const colors = require('colors');
+const config = require('./config.json')
+const client = new Discord.Client();
+
+client.on("ready", () => {
+    console.log(colors.cyan(`
+
+
+
+            ▄████████  ▄█          ▄████████    ▄████████    ▄████████ 
+            ███    ███ ███         ███    ███   ███    ███   ███    ███ 
+            ███    █▀  ███         ███    █▀    ███    ███   ███    ███ 
+            ███        ███        ▄███▄▄▄       ███    ███  ▄███▄▄▄▄██▀ 
+            ███        ███       ▀▀███▀▀▀     ▀███████████ ▀▀███▀▀▀▀▀   
+            ███    █▄  ███         ███    █▄    ███    ███ ▀███████████ 
+            ███    ███ ███▌    ▄   ███    ███   ███    ███   ███    ███ 
+            ████████▀  █████▄▄██   ██████████   ███    █▀    ███    ███ 
+                       ▀                                     ███    ███ 
+                       `));
+     console.log(colors.white(`                   DM Clear connected in: ${client.user.username}`));
+});
+
+client.on("message", async (message) => {
+
+    if (message.author.id == client.user.id && message.content.startsWith(".purge")) { // Here you can change the command to clear messages
+        const max = 50 // Each 1 is 100, so 50 are the last 5000 messages
+        let lastMessageID;
+        for (let a = 0; a < max; a++) {
+            const messages = await message.channel.fetchMessages({ limit: 10, before: lastMessageID });
+            lastMessageID = messages.last().id;
+
+            const mymessages = messages.filter(m => m.author.id === message.author.id).array()
+
+            console.log(colors.green(`  [$] - Message deleted`));
+
+            for (let msg of mymessages) {
+                await msg.delete().catch(() => { })
+            }
+        }
+    }
+});
+
+client.login(config.token);
---
+++ start.bat
@@ -0,0 +1,8 @@
+color 0
+title DM Clear - angelo
+@echo off
+cls
+:top
+node Main.js
+pause
+goto top
